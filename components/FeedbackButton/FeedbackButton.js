bf86ebb1027a5b6f946a7c28bc88f68bf4b02a4a
Tracy <treyhallp@gmail.com>
2026-05-07 17:45:53 -0400

    capitalization

--- components/FeedbackButton/FeedbackButton.js
+++ components/FeedbackButton/FeedbackButton.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
-import styles from "./FeedbackButton.module.css";
+import styles from "./FeedbackButton.Module.css";
 
 export default function FeedbackButton() {
   const pathname = usePathname();
